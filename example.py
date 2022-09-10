ef9606a6e8e66db051af5045deb6ba8392fb70b8
Abdalla Rashed <r.abdalla.1195@gmail.com>
2022-09-10 18:42:13 +0400

    create TabularModel.__init__ and utils.custom_layers.outLayer1d

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from src.custom_preprocess import process_nyc_dataset
 from src.preprocess import pd_to_torch
+from src.model import TabularModel
 
 # Data Loading and Pre-Processing
 nyc = pd.read_csv('./data/NYCTaxiFares.csv')
@@ -12,6 +13,11 @@ continuous_features = ['pickup_longitude', 'pickup_latitude', 'dropoff_longitude
                        'dropoff_latitude', 'passenger_count', 'dist_km']
 y_feature = 'fare_amount'
 
+classification = False
+
 # Loading data as tensors. Tensors are created on a CUDA device if one is available
-cat, cont, y = pd_to_torch(nyc, categorical_features, continuous_features, y_feature, gpu=True)
+cat, cont, y = pd_to_torch(nyc, categorical_features, continuous_features, y_feature,
+                           categorical_target=classification, gpu=True)
+
+model = TabularModel(layers=[], classification=classification)
 
--- src/model.py
+++ src/model.py
@@ -1,9 +1,39 @@
 import torch
 import torch.nn as nn
 import numpy as np
+import warnings
+from dataclasses import dataclass
+from utils.custom_layers import outLayer1d
 
 
 class TabularModel(nn.Module):
 
-    def __init__(self):
+    def configure(self, activation=nn.ReLU, batch_norm=True, dropout=0.05):
+        @dataclass
+        class configs:
+            cfg_activation = activation
+            cfg_batch_norm = nn.BatchNorm1d if batch_norm else lambda x: x
+            cfg_dropout = nn.Dropout(dropout)
+
+        self.configs = configs
+
+    def __init__(self, layers, out_size=1, classification=False):
         super().__init__()
+        self.is_fit = False
+        self.configure()
+
+        self.hidden_layers = []
+
+        warnings.filterwarnings("ignore", message="Initializing zero-element tensors is a no-op")
+        in_features = 0
+
+        for out_features in layers:
+            self.hidden_layers.append(nn.Linear(in_features, out_features))
+            self.hidden_layers.append(self.configs.cfg_activation(inplace=True))
+            self.hidden_layers.append(self.configs.cfg_batch_norm(out_features))
+            self.hidden_layers.append(self.configs.cfg_dropout)
+            in_features = out_features
+
+        activation = nn.Softmax() if classification else None
+        self.out_layer = outLayer1d(in_features, out_size, activation=activation)
+
